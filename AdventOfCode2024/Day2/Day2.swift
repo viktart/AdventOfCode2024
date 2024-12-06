cb6b827ce191b38fe952d70c233e6070f35ef2b8
Viktar T <meandmymouse@gmail.com>
2024-12-06 18:20:30 +0100

    Day 2 Part Two

--- AdventOfCode2024/Day2/Day2.swift
+++ AdventOfCode2024/Day2/Day2.swift
@@ -7,39 +7,89 @@
 
 import Foundation
 
-func dayTwoPartOne(_ inputFileName: String) -> Int {
-    let fileContent = readFile(inputFileName)
-    let numbers = parseLinesOfNumbers(fileContent)
-    let result = numbers.reduce(0) { partialResult, line in
+class DayTwo {
+    
+    static private func findFirstUnsafeIndex(_ line: [Int]) -> Int? {
+        findFirstUnsafeIndex(line[0..<line.count])
+    }
+    
+    static private func findFirstUnsafeIndex(_ line: ArraySlice<Int>) -> Int? {
         guard !line.isEmpty else {
-            return partialResult
+            return -1
         }
         guard line.count > 1 else {
-            return partialResult + 1
+            return nil
         }
-        guard line[0] != line[1] else {
-            return partialResult
-        }
-        
-        let isAscending = line[0] < line[1]
-        if abs(line[0] - line[1]) > 3 {
-            return partialResult
+        guard line[line.startIndex] != line[line.startIndex + 1] else {
+            return line.startIndex
         }
         
-        for i in 2..<line.count {
+        let isAscending = line[line.startIndex] < line[line.startIndex + 1]
+        if abs(line[line.startIndex] - line[line.startIndex + 1]) > 3 {
+            return line.startIndex
+        }
+        
+        for i in line.startIndex + 2 ..< line.endIndex {
             if line[i - 1] == line[i] {
-                return partialResult
+                return i - 1
             }
             let currentAscending = line[i - 1] < line[i]
             if isAscending != currentAscending {
-                return partialResult
+                return i - 1
             }
             if abs(line[i - 1] - line[i]) > 3 {
-                return partialResult
+                return i - 1
             }
         }
         
-        return partialResult + 1
+        return nil
     }
-    return result
+    
+    static func partOne(_ inputFileName: String) -> Int {
+        let fileContent = readFile(inputFileName)
+        let numbers = parseLinesOfNumbers(fileContent)
+        let result = numbers.reduce(0) { partialResult, line in
+            return findFirstUnsafeIndex(line) == nil ? partialResult + 1 : partialResult
+        }
+        return result
+    }
+    
+    static func partTwo(_ inputFileName: String) -> Int {
+        let fileContent = readFile(inputFileName)
+        let numbers = parseLinesOfNumbers(fileContent)
+        let result = numbers.reduce(0) { partialResult, line in
+            
+            func excludeAndCheck(index: Int) -> Bool {
+                var newLine = line
+                newLine.remove(at: index)
+                let unsafeIndex = findFirstUnsafeIndex(newLine)
+                return unsafeIndex == nil
+            }
+            
+            if findFirstUnsafeIndex(line) == nil {
+                return partialResult + 1
+            }
+            if findFirstUnsafeIndex(line[1..<line.count]) == nil {
+                return partialResult + 1
+            }
+            if excludeAndCheck(index: line.startIndex + 1) {
+                return partialResult + 1
+            }
+            
+            guard let unsafeIndex = findFirstUnsafeIndex(line) else {
+                fatalError("Programming error")
+            }
+            guard unsafeIndex > line.startIndex else {
+                return partialResult
+            }
+            
+            if excludeAndCheck(index: unsafeIndex) || excludeAndCheck(index: unsafeIndex + 1) {
+                return partialResult + 1
+            }
+
+            return partialResult
+        }
+        
+        return result
+    }
 }
--- AdventOfCode2024/main.swift
+++ AdventOfCode2024/main.swift
@@ -48,5 +48,6 @@ func readDayOneInputFromFile(_ fileName: String) -> ([Int], [Int]) {
 
 //let result = dayOne(inputFileName: "Day1_1.txt")
 //let result = dayOnePartTwo(inputFileName: "Day1_1.txt")
-let result = dayTwoPartOne("Day2_1.txt")
+//let result = DayTwo.partOne("Day2_1.txt")
+let result = DayTwo.partTwo("Day2_1.txt")
 print(result)
